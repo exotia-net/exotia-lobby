b2b955c53a3c5002c77ae8b41a598571419641a1
botprzemek <info@botprzemek.pl>
2023-12-06 19:49:14 +0100

    Fixed chat message

--- src/main/java/net/exotia/plugins/lobby/listener/ListenerChat.java
+++ src/main/java/net/exotia/plugins/lobby/listener/ListenerChat.java
@@ -1,11 +1,14 @@
 package net.exotia.plugins.lobby.listener;
 
 import eu.okaeri.injector.annotation.Inject;
+import io.papermc.paper.chat.ChatRenderer;
 import io.papermc.paper.event.player.AsyncChatEvent;
 import me.clip.placeholderapi.PlaceholderAPI;
 import net.exotia.plugins.lobby.configuration.ConfigurationMessage;
 import net.exotia.plugins.lobby.configuration.ConfigurationPlugin;
 import net.exotia.plugins.lobby.utils.UtilMessage;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -28,6 +31,12 @@ public class ListenerChat implements Listener {
         }
 
         String color = configurationPlugin.getFormats().get(PlaceholderAPI.setPlaceholders(player, "%vault_group%")) != null ? configurationPlugin.getFormats().get(PlaceholderAPI.setPlaceholders(player, "%vault_group%")) : "";
-        event.message(UtilMessage.getComponent(player, configurationMessage.getEventsChat().getSuccess(), player.getDisplayName(), color, event.message().toString()));
+        event.renderer(((source, sourceDisplayName, message, viewer) -> UtilMessage.getComponent(
+            source,
+            configurationMessage.getEventsChat().getSuccess(),
+            MiniMessage.miniMessage().serialize(sourceDisplayName),
+            color,
+            MiniMessage.miniMessage().serialize(message))
+        ));
     }
 }
